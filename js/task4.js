3e55fd37ca177a2d86c0065e5fbaa350499a813b
imransarkerweb <imransarker.web@gmail.com>
2023-02-28 19:46:31 +0600

    shorten project started

---
+++ js/task4.js
@@ -0,0 +1,7 @@
+//variables 
+const inputField = document.getElementById('input-field')
+const shortenBtn = document.getElementById("shorten-btn")
+const showAllLinkBtn = document.getElementById("show-allLink-btn")
+const copyLinkBtn = document.getElementById('copy-link')
+
+const showShortenLink = document.getElementById("shorten-link")
